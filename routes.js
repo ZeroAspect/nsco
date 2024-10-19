ae48596f28ffef4f499fa619c9fbdde310f6abda
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-10-18 22:13:38 -0300

    feat: implement static files

--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const app = require("./config/config.js")
-
+const hbs = require("express-handlebars")
+const path = require("path")
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Static files
+app.engine("handlebars", hbs.engine())
+app.set("view engine", "handlebars")
+app.set("views", path.join(__dirname + "/views"))
+
 // Routes
 app.get('/', async(req, res)=>{
-  res.send("Hello World!")
+  res.render('home')
 })
