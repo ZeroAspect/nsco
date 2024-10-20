ae451a06b1138a4438e4c22504001b0358785107
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-10-20 16:23:24 -0300

    feat: creating new routes

--- routes.js
+++ routes.js
@@ -5,6 +5,9 @@ const path = require("path")
 const User = require("./users/getUser.js")
 const GetIP = require("./infra/ip.js")
 const { marked } = require("marked")
+const Post = require("./post/Post.js")
+const db = require("./sequelize/sequelize.js")
+const MySQL = require("./db/initial-db.js")
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -16,6 +19,7 @@ app.set("views", path.join(__dirname + "/views"))
 
 // Routes
 app.get('/', async(req, res)=>{
+  const mysql = await MySQL()
   const ip = await GetIP()
   console.log(ip)
   try{
@@ -27,7 +31,9 @@ app.get('/', async(req, res)=>{
     if(user === null){
       res.redirect('/login')
     } else{
-      res.render('home')
+      const [ posts, rows ] = await mysql.query(`SELECT * FROM Posts ORDER BY post_like DESC`)
+      console.log(posts)
+      res.render('home', { posts })
     }
   } catch(error) {
     console.error(error)
@@ -142,6 +148,52 @@ app.get('/publicar', async(req, res)=>{
       res.render('publicar')
     }
   } catch(error) {
+    console.error(error)
+    res.status(500).send('Server error')
+  }
+})
+app.get('/publicar/conteudo', async(req, res)=>{
+  const ip = await GetIP()
+  try{
+    const user = await User.findOne({
+      where: {
+        ip: ip.ip
+      }
+    })
+    if(user === null){
+      res.redirect('/login')
+    } else{
+      res.render('publicar/conteudo', { user: user['nome'] })
+    }
+  } catch(error) {
+    console.error(error)
+    res.status(500).send('Server error')
+  }
+})
+app.post('/publicar/conteudo', async(req, res)=>{
+  const ip = await GetIP()
+  const { titulo, conteudo, fonte } = req.body
+  console.log(titulo, conteudo)
+  const user = await User.findOne({
+    where: {
+      ip: ip.ip
+    }
+  })
+  try {
+    if(user === null){
+      res.redirect('/login')
+    } else{
+      const post = await Post.create({
+        nome: user['nome'],
+        titulo: titulo,
+        conteudo: marked(conteudo),
+        fonte: fonte,
+        // post_like: '0'
+      })
+      console.log(post)
+      res.redirect('/')
+    }
+  } catch (error) {
     console.error(error)
     res.status(500).send('Server error')
   }
